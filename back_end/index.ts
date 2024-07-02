7b1dd0a9bba49fa02e542384d720b536f55843cd
Alexander Biryukov <74318440+AlexFromNorth@users.noreply.github.com>
2024-07-02 20:26:08 +0300

    added auto swagger

--- back_end/index.ts
+++ back_end/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import folderRoutes from "./routes/folder.routes"
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger-output.json');
 
 const app = express();
 app.use(express.json());
 app.use("/folder", folderRoutes)
 
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3000); //port
