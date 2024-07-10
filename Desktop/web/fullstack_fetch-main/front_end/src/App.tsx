3dffe705d916e28dc6d2d1ee91f8abe6f54e7b1c
Alexander Biryukov <74318440+AlexFromNorth@users.noreply.github.com>
2024-07-10 15:34:35 +0300

    debug commits

--- Desktop/web/fullstack_fetch-main/front_end/src/App.tsx
+++ Desktop/web/fullstack_fetch-main/front_end/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
         const data = await response.json();
         console.log(data);
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching data:", error);  
       }
     };
 
